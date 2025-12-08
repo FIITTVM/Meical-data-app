f326883748890f32d9c355216ecdc8092a96fbb6
Vijayalakshmi <vijayalakshmi@gmail.com>
2025-12-08 19:45:34 +0530

    Update settings and WSGI for deployment

--- medicalhistory/settings.py
+++ medicalhistory/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-8#random$secret!key'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
